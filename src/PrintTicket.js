2660a8d05609824c71891f1b6d32bf966fd0192c
Παναγιώτης Κουλουμαριέτος <panoskoul108@gmail.com>
2026-03-02 21:27:16 +0000

    neo tickets

--- src/PrintTicket.js
+++ src/PrintTicket.js
@@ -7,7 +7,7 @@ export const PrintTicket = ({ order }) => {
     <div
       style={{
         width: "80mm",
-        padding: "5mm",
+        padding: "0px", // Μηδενισμός κενού για οικονομία χαρτιού
         backgroundColor: "white",
         color: "black",
         fontFamily: "monospace",
@@ -17,37 +17,41 @@ export const PrintTicket = ({ order }) => {
         style={{
           textAlign: "center",
           borderBottom: "1px dashed black",
-          paddingBottom: "10px",
+          paddingBottom: "8px",
+          marginTop: "0px",
         }}
       >
-        <h2 style={{ margin: 0, fontSize: "18px", fontWeight: "bold" }}>
-          STATUS ΚΟΥΖΙΝΑ
+        <h2 style={{ margin: 0, fontSize: "20px", fontWeight: "bold" }}>
+          STATUS
         </h2>
-        <p style={{ margin: "5px 0", fontSize: "10px" }}>
+        {/* Η ΩΡΑ ΜΕΓΑΛΩΣΕ ΕΔΩ */}
+        <p style={{ margin: "5px 0", fontSize: "14px", fontWeight: "bold" }}>
           {new Date(order.created_at).toLocaleString("el-GR")}
         </p>
       </div>
 
-      <div style={{ textAlign: "center", margin: "15px 0" }}>
-        <h1 style={{ fontSize: "32px", margin: 0, fontWeight: "900" }}>
+      <div style={{ textAlign: "center", margin: "10px 0" }}>
+        <h1 style={{ fontSize: "40px", margin: 0, fontWeight: "900" }}>
           {order.table_number}
         </h1>
       </div>
 
       <table style={{ width: "100%", borderCollapse: "collapse" }}>
-        {order.items.map((item, i) => (
-          <tr key={i} style={{ fontSize: "16px", fontWeight: "bold" }}>
-            <td style={{ padding: "5px 0" }}>• {item.name}</td>
-            <td style={{ textAlign: "right" }}>1x</td>
-          </tr>
-        ))}
+        <tbody>
+          {order.items.map((item, i) => (
+            <tr key={i} style={{ fontSize: "18px", fontWeight: "bold" }}>
+              <td style={{ padding: "4px 0" }}>• {item.name}</td>
+              <td style={{ textAlign: "right" }}>{item.quantity || 1}x</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
 
       <div
         style={{
           borderTop: "2px solid black",
-          marginTop: "15px",
-          paddingTop: "10px",
+          marginTop: "10px",
+          paddingTop: "8px",
         }}
       >
         <div
@@ -63,12 +67,12 @@ export const PrintTicket = ({ order }) => {
         </div>
         <div
           style={{
-            marginTop: "10px",
+            marginTop: "8px",
             fontSize: "14px",
             fontWeight: "bold",
             textAlign: "center",
             border: "1px solid black",
-            padding: "5px",
+            padding: "4px",
           }}
         >
           ΠΛΗΡΩΜΗ: {order.payment_method}
