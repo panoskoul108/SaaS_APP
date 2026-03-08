a8b61cf0d9052f9c7ab39a2615a34649da545816
Παναγιώτης Κουλουμαριέτος <panoskoul108@gmail.com>
2026-03-08 11:50:54 +0000

    πριντ

--- src/PrintTicket.js
+++ src/PrintTicket.js
@@ -7,73 +7,148 @@ export const PrintTicket = ({ order }) => {
     <div
       style={{
         width: "80mm",
-        padding: "5mm",
+        padding: "0px",
+        margin: "0px",
         backgroundColor: "white",
         color: "black",
         fontFamily: "monospace",
+        lineHeight: "1.1", // Μειώνει το κενό ανάμεσα σε όλες τις γραμμές
       }}
     >
+      {/* 1. HEADER: ΤΡΑΠΕΖΙ & ΩΡΑ ΣΤΗΝ ΙΔΙΑ ΓΡΑΜΜΗ */}
       <div
         style={{
-          textAlign: "center",
-          borderBottom: "1px dashed black",
-          paddingBottom: "10px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+          borderBottom: "2px dashed black",
+          paddingBottom: "4px",
+          marginBottom: "4px",
         }}
       >
-        <h2 style={{ margin: 0, fontSize: "18px", fontWeight: "bold" }}>
-          STATUS ΚΟΥΖΙΝΑ
-        </h2>
-        <p style={{ margin: "5px 0", fontSize: "10px" }}>
-          {new Date(order.created_at).toLocaleString("el-GR")}
-        </p>
+        <div style={{ textAlign: "left" }}>
+          <h2 style={{ margin: 0, fontSize: "14px", fontWeight: "bold" }}>
+            STATUS
+          </h2>
+          <h1
+            style={{
+              margin: 0,
+              fontSize: "36px",
+              fontWeight: "900",
+              lineHeight: "1",
+            }}
+          >
+            #{order.table_number}
+          </h1>
+        </div>
+        <div style={{ textAlign: "right" }}>
+          <p style={{ margin: 0, fontSize: "16px", fontWeight: "900" }}>
+            {new Date(order.created_at).toLocaleString("el-GR", {
+              dateStyle: "short",
+              timeStyle: "short",
+            })}
+          </p>
+        </div>
       </div>
 
-      <div style={{ textAlign: "center", margin: "15px 0" }}>
-        <h1 style={{ fontSize: "32px", margin: 0, fontWeight: "900" }}>
-          {order.table_number}
-        </h1>
-      </div>
-
+      {/* 2. ΠΡΟΪΟΝΤΑ & ΣΗΜΕΙΩΣΕΙΣ (Χωρίς περιττά κενά) */}
       <table style={{ width: "100%", borderCollapse: "collapse" }}>
-        {order.items.map((item, i) => (
-          <tr key={i} style={{ fontSize: "16px", fontWeight: "bold" }}>
-            <td style={{ padding: "5px 0" }}>• {item.name}</td>
-            <td style={{ textAlign: "right" }}>1x</td>
-          </tr>
-        ))}
+        <tbody>
+          {order.items.map((item, i) => (
+            <tr
+              key={i}
+              style={{ borderBottom: item.note ? "1px dotted #ccc" : "none" }}
+            >
+              <td style={{ padding: "2px 0", verticalAlign: "top" }}>
+                <span style={{ fontSize: "20px", fontWeight: "bold" }}>
+                  • {item.name}
+                </span>
+                {/* Η σημείωση μπήκε ΑΚΡΙΒΩΣ κάτω από το προϊόν, στο ίδιο κελί */}
+                {item.note && (
+                  <div
+                    style={{
+                      fontSize: "16px",
+                      fontWeight: "900",
+                      fontStyle: "italic",
+                      marginTop: "1px",
+                    }}
+                  >
+                    👉 ΣΗΜ: {item.note}
+                  </div>
+                )}
+              </td>
+              <td
+                style={{
+                  textAlign: "right",
+                  verticalAlign: "top",
+                  paddingTop: "2px",
+                  fontSize: "20px",
+                  fontWeight: "bold",
+                  minWidth: "30px",
+                }}
+              >
+                {item.quantity || 1}x
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
 
+      {/* 3. ΓΕΝΙΚΗ ΣΗΜΕΙΩΣΗ (Πιο συμπαγής) */}
+      {order.general_note && (
+        <div
+          style={{
+            border: "2px solid black",
+            padding: "2px 4px",
+            marginTop: "4px",
+            fontSize: "16px",
+            fontWeight: "900",
+          }}
+        >
+          <span style={{ textDecoration: "underline" }}>ΓΕΝ. ΣΗΜ:</span>{" "}
+          {order.general_note}
+        </div>
+      )}
+
+      {/* 4. FOOTER: ΣΥΝΟΛΟ ΚΑΙ ΠΛΗΡΩΜΗ ΣΕ 1 ΓΡΑΜΜΗ */}
       <div
         style={{
           borderTop: "2px solid black",
-          marginTop: "15px",
-          paddingTop: "10px",
+          marginTop: "4px",
+          paddingTop: "4px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            fontSize: "18px",
-            fontWeight: "bold",
-          }}
-        >
-          <span>ΣΥΝΟΛΟ:</span>
-          <span>{order.total_price.toFixed(2)}€</span>
+        <div style={{ fontSize: "14px", fontWeight: "bold" }}>
+          ΠΛΗΡΩΜΗ:{" "}
+          <span style={{ fontSize: "16px", fontWeight: "900" }}>
+            {order.payment_method}
+          </span>
         </div>
-        <div
-          style={{
-            marginTop: "10px",
-            fontSize: "14px",
-            fontWeight: "bold",
-            textAlign: "center",
-            border: "1px solid black",
-            padding: "5px",
-          }}
-        >
-          ΠΛΗΡΩΜΗ: {order.payment_method}
+        <div style={{ fontSize: "16px", fontWeight: "bold" }}>
+          ΣΥΝ:{" "}
+          <span style={{ fontSize: "20px", fontWeight: "900" }}>
+            {order.total_price.toFixed(2)}€
+          </span>
         </div>
       </div>
+
+      {/* LOYALTY (Μικρότερο margin) */}
+      {order.is_loyalty_reward && (
+        <div
+          style={{
+            marginTop: "4px",
+            border: "2px solid black",
+            textAlign: "center",
+            fontWeight: "bold",
+            padding: "2px",
+          }}
+        >
+          🎁 ΔΩΡΟ LOYALTY 🎁
+        </div>
+      )}
     </div>
   );
 };
